a7a6a97c2fc499ccf39f3ddca15ffb020eb854a2
fengxc <fengxiaochen@gmail.com>
2020-02-21 20:46:30 +0800

    add export test

--- app/src/main/java/com/example/tryproj/MainActivity.java
+++ app/src/main/java/com/example/tryproj/MainActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -58,7 +62,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         exportrButton = findViewById(R.id.buttonexport);
+        exportrButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String filecontentText ="";
+                for(int index=0;index<mData.size();index++){
+                    filecontentText+=mData.get(index).getItemName()+","+mData.get(index).getLocateString()+","+mData.get(index).isStatus()+"\r\n";
+                }
+                String filename = "Qone/"+System.currentTimeMillis()+".txt";
+                try {
+                    //判断SDcard是否存在并且可读写
+                    if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+                        saveToSDCard(filename,filecontentText);
+                        Toast.makeText(getApplicationContext(), R.string.success, Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getApplicationContext(), R.string.sdcarderror, Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
 
     }
 
+    public void saveToSDCard(String filename, String content)throws Exception {
+        /*
+         * 保存文件到sd卡，sd卡用于保存大文件（视频，音乐，文档等）
+         * 获取sd卡路径Environment.getExternalStorageDirectory()
+         * android版本不同，sd卡的路径也不相同，所以这里不能写绝对路径
+         * */
+        File file = new File(Environment.getExternalStorageDirectory(), filename);
+        FileOutputStream outStream = new FileOutputStream(file);
+
+
+        outStream.write(content.getBytes());
+        outStream.close();
+    }
 }
